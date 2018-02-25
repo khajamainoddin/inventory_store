4acdf399333d97c4d1cb69a55e7fd6925b3bd9ed
khajamainoddin <khajamainoddin.mohd@gmail.com>
2018-02-25 19:47:37 +0530

    this is for testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'arcane-spire-69969.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: "https://arcane-spire-69969.herokuapp.com" }
+
+
+#  config.action_mailer.default_url_options = { host: "localhost:3000" }
 
 end
