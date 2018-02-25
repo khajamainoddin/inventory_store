48c0acfed55d27cfe421a9b0a9bb0d8f726b8200
khajamainoddin <khajamainoddin.mohd@gmail.com>
2018-02-25 21:44:23 +0530

    production file is changed

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,5 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,9 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "arcane-spire-69969.herokuapp.com" }
-
-
-#  config.action_mailer.default_url_options = { host: "localhost:3000" }
+config.action_mailer.default_url_options = { :host => 'arcane-spire-69969.herokuapp.com' }
 
 end
